fa15eb948fecdc4c8673c2ef11e0c6a26367b825
zhouxu <zhouxu@simpretech.com>
2018-05-30 09:25:11 +0800

    add Splash Screen

--- QtLearning/src/main.cpp
+++ QtLearning/src/main.cpp
@@ -1,11 +1,22 @@
 #include <QApplication>
+#include <QSplashScreen>
 #include "dialog.h"
+
+
+
+
 int main(int argc,char* argv[])
 {
     QApplication app(argc, argv);
-
+    QPixmap pix("C://Users/Nikola/Desktop/1.jpg");
+    
+    QSplashScreen* splash = new QSplashScreen(pix);
+    splash->show();
+    qApp->processEvents();
+    splash->showMessage(QObject::tr("I am your father"));
     ExtendWidnow w;
+    splash->finish(&w);
     w.show();
-
+    delete splash;
     return app.exec();
 }
